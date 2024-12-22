e6884c1622b88d328eb0fd44e831112e6cec90f0
GeorgiDN <go6odn28@gmail.com>
2024-12-22 14:54:24 +0200

    feat: add postdeleteview and add links to navigation

--- webApp/webApp/blog/views.py
+++ webApp/webApp/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from webApp.blog.models import Post
 
@@ -43,6 +43,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
