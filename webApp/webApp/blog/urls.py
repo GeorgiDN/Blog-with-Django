3daeff15b0245f60d7ac64542316012bb90e13ce
GeorgiDN <go6odn28@gmail.com>
2024-12-22 12:09:04 +0200

    feat: add postlistview and postdetailview

--- webApp/webApp/blog/urls.py
+++ webApp/webApp/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from webApp.blog.views import PostListView, PostDetailView
 from webApp.blog import views
 
+
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
--- webApp/webApp/blog/views.py
+++ webApp/webApp/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+
 from webApp.blog.models import Post
 
 
@@ -11,6 +13,17 @@ def home(request):
     }
 
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
