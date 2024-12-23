656df6fc8a6be53500c60a250951b98932d72086
GeorgiDN <go6odn28@gmail.com>
2024-12-23 08:10:58 +0200

    feat: log users in automatically upon registration

--- webApp/webApp/users/views.py
+++ webApp/webApp/users/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib import messages
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
 from webApp.users.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has with username {username} been created! You are now able to log in')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
+class AppUserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('blog-home')
 
-    context = {
-        'form': form
-    }
+    def form_valid(self, form):
+        response = super().form_valid(form)
 
-    return render(request, 'users/register.html', context)
+        login(self.request, self.object)
+        messages.success(self.request, 'Your account has been created!')
+        return response
 
 
 @login_required
@@ -46,3 +47,20 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
+# FBV
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Your account has with username {username} been created!')
+#             return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#
+#     context = {
+#         'form': form
+#     }
+#
+#     return render(request, 'users/register.html', context)
