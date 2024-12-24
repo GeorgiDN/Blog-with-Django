336575467d152aa8d65c227b0fec4d2b0819083f
GeorgiDN <go6odn28@gmail.com>
2024-12-24 15:12:45 +0200

    feat: add toggleComments function

---
+++ webApp/static/blog/js/toggleComments.js
@@ -0,0 +1,10 @@
+function toggleComments(postId) {
+    const commentsDiv = document.getElementById(`comments-${postId}`);
+    if (commentsDiv.style.display === "none") {
+        commentsDiv.style.display = "block";
+    } else {
+        commentsDiv.style.display = "none";
+    }
+}
+
+
