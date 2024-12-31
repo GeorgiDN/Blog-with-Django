0ae2210d9fbdee7b4aba04ab17ec9268a04f7d5e
GeorgiDN <go6odn28@gmail.com>
2024-12-31 11:49:31 +0200

    feat:(messaging) add profile image in message template

--- webApp/webApp/messaging/views.py
+++ webApp/webApp/messaging/views.py
@@ -8,10 +8,6 @@ from .models import Message
 @login_required
 def conversations_list(request):
     user = request.user
-    # Get users with whom the user has exchanged messages
-    # conversations = User.objects.filter(
-    #     Q(sent_messages__recipient=user) | Q(received_messages__sender=user)
-    # ).distinct()
 
     conversations = User.objects.filter(
         Q(sent_messages__recipient=user) | Q(received_messages__sender=user)
@@ -21,13 +17,6 @@ def conversations_list(request):
             filter=Q(sent_messages__recipient=user, sent_messages__is_read=False)
         )
     )
-
-    # unread_counts = {}
-    # for conversation_user in conversations:
-    #     unread_count = Message.objects.filter(
-    #         recipient=user, sender=conversation_user, is_read=False
-    #     ).count()
-    #     unread_counts[conversation_user.username] = unread_count
 
     context = {
         'conversations': conversations,
@@ -44,20 +33,14 @@ def conversation_detail(request, username):
     sender = User.objects.get(username=username)
 
     # Fetch messages exchanged between the two users
-    messages = Message.objects.filter(
+    conversation_messages = Message.objects.filter(
         Q(sender=user, recipient=recipient) | Q(sender=recipient, recipient=user)
     ).order_by('timestamp')
 
-    messages.filter(recipient=user, is_read=False).update(is_read=True)
+    conversation_messages.filter(recipient=user, is_read=False).update(is_read=True)
 
     incoming_messages = Message.objects.filter(Q(sender=recipient, recipient=user))
     sent_messages = Message.objects.filter(Q(sender=recipient, recipient=user))
-
-    unread_count_by_user = Message.objects.filter(
-        recipient=user,
-        sender=sender,
-        is_read=False
-    ).count()
 
     if request.method == "POST":
         content = request.POST.get('content')
@@ -66,8 +49,7 @@ def conversation_detail(request, username):
 
     context = {
         'recipient': recipient,
-        'messages': messages,
-        'unread_count_by_user': unread_count_by_user,
+        'conversation_messages': conversation_messages,
         'incoming_messages': incoming_messages,
         'sent_messages': sent_messages
     }
