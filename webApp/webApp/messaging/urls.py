9b6f4080a0be288ac391c1b0ddd9c8ee76a8b0d2
GeorgiDN <go6odn28@gmail.com>
2025-01-01 13:40:28 +0200

    refactor(messages): replace function-based views with class-based views for conversations

--- webApp/webApp/messaging/urls.py
+++ webApp/webApp/messaging/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from webApp.messaging import views
 
 urlpatterns = [
-    path('messages/', views.conversations_list, name='conversations-list'),
-    path('messages/<str:username>/', views.conversation_detail, name='conversation-detail'),
+    path('messages/', views.ConversationsListView.as_view(), name='conversations-list'),
+    path('messages/<str:username>/', views.ConversationDetailView.as_view(), name='conversation-detail'),
     path('messages/edit/<int:pk>/', views.MessageEditView.as_view(), name='edit-message'),
     path('messages/delete/<int:pk>/', views.MessageDeleteView.as_view(), name='delete-message'),
 ]
--- webApp/webApp/messaging/views.py
+++ webApp/webApp/messaging/views.py
@@ -1,59 +1,66 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Count
 from django.urls.base import reverse
-from django.views.generic import UpdateView, DeleteView
+from django.views.generic import UpdateView, DeleteView, ListView, DetailView
 
 from .forms import MessageEditForm
 from .models import Message
 
 
-@login_required
-def conversations_list(request):
-    user = request.user
+class ConversationsListView(LoginRequiredMixin, ListView):
+    model = User
+    template_name = 'messaging/conversations_list.html'
+    context_object_name = 'conversations'
 
-    conversations = User.objects.filter(
-        Q(sent_messages__recipient=user) | Q(received_messages__sender=user)
-    ).distinct().annotate(
-        unread_count=Count(
-            'sent_messages',
-            filter=Q(sent_messages__recipient=user, sent_messages__is_read=False)
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(
+            Q(sent_messages__recipient=user) | Q(received_messages__sender=user)
+        ).distinct().annotate(
+            unread_count=Count(
+                'sent_messages',
+                filter=Q(sent_messages__recipient=user, sent_messages__is_read=False)
+            )
         )
-    )
-
-    context = {
-        'conversations': conversations,
-
-    }
-
-    return render(request, 'messaging/conversations_list.html', context)
 
 
-@login_required
-def conversation_detail(request, username):
-    user = request.user
-    recipient = User.objects.get(username=username)
+class ConversationDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'messaging/conversation_detail.html'
+    context_object_name = 'recipient'
 
-    # Fetch messages exchanged between the two users
-    conversation_messages = Message.objects.filter(
-        Q(sender=user, recipient=recipient) | Q(sender=recipient, recipient=user)
-    ).order_by('timestamp')
+    def get_object(self, **kwargs):
+        return User.objects.get(username=self.kwargs['username'])
 
-    conversation_messages.filter(recipient=user, is_read=False).update(is_read=True)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        recipient = self.get_object()
 
-    if request.method == "POST":
+        conversation_messages = Message.objects.filter(
+            Q(sender=user, recipient=recipient) | Q(sender=recipient, recipient=user)
+        ).order_by('timestamp')
+
+        conversation_messages.filter(recipient=user, is_read=False).update(is_read=True)
+
+        context['conversation_messages'] = conversation_messages
+        return context
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        recipient = self.get_object()
         content = request.POST.get('content')
+
         if content:
             Message.objects.create(sender=user, recipient=recipient, content=content)
 
-    context = {
-        'recipient': recipient,
-        'conversation_messages': conversation_messages,
-    }
+        return redirect(self.get_success_url())
 
-    return render(request, 'messaging/conversation_detail.html', context)
+    def get_success_url(self):
+        return reverse('conversation-detail', kwargs={'username': self.get_object().username})
 
 
 class MessageEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -79,3 +86,50 @@ class MessageDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         curr_message = self.get_object()
         return curr_message.sender == self.request.user
+
+
+# FBV
+# @login_required
+# def conversations_list(request):
+#     user = request.user
+#
+#     conversations = User.objects.filter(
+#         Q(sent_messages__recipient=user) | Q(received_messages__sender=user)
+#     ).distinct().annotate(
+#         unread_count=Count(
+#             'sent_messages',
+#             filter=Q(sent_messages__recipient=user, sent_messages__is_read=False)
+#         )
+#     )
+#
+#     context = {
+#         'conversations': conversations,
+#
+#     }
+#
+#     return render(request, 'messaging/conversations_list.html', context)
+#
+#
+# @login_required
+# def conversation_detail(request, username):
+#     user = request.user
+#     recipient = User.objects.get(username=username)
+#
+#     # Fetch messages exchanged between the two users
+#     conversation_messages = Message.objects.filter(
+#         Q(sender=user, recipient=recipient) | Q(sender=recipient, recipient=user)
+#     ).order_by('timestamp')
+#
+#     conversation_messages.filter(recipient=user, is_read=False).update(is_read=True)
+#
+#     if request.method == "POST":
+#         content = request.POST.get('content')
+#         if content:
+#             Message.objects.create(sender=user, recipient=recipient, content=content)
+#
+#     context = {
+#         'recipient': recipient,
+#         'conversation_messages': conversation_messages,
+#     }
+#
+#     return render(request, 'messaging/conversation_detail.html', context)
