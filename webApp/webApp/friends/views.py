45edd0985c05a9fc9d2cf7b6a7aacb8a8db61648
GeorgiDN <go6odn28@gmail.com>
2025-01-07 10:58:31 +0200

    feat:(friends): add styles to friend section

--- webApp/webApp/friends/views.py
+++ webApp/webApp/friends/views.py
@@ -4,18 +4,34 @@ from django.contrib.auth.decorators import login_required
 from .models import Friendship, FriendRequest
 from django.contrib.auth.models import User
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Friendship, FriendRequest
+from django.contrib.auth.models import User
+
 
 @login_required
 def send_friend_request(request, user_id):
     to_user = get_object_or_404(User, id=user_id)
-    if request.user != to_user:
-        friend_request, created = FriendRequest.objects.get_or_create(
-            from_user=request.user, to_user=to_user
-        )
-        if created:
-            messages.success(request, f'Friend request sent to {to_user.username}.')
-        else:
-            messages.info(request, f'You have already sent a friend request to {to_user.username}.')
+
+    if request.user == to_user:
+        messages.error(request, "You cannot send a friend request to yourself.")
+        return redirect('profile-view', username=to_user.username)
+
+    if Friendship.objects.filter(user=request.user, friends=to_user).exists():
+        messages.info(request, f'You are already friends with {to_user.username}.')
+        return redirect('profile-view', username=to_user.username)
+
+    friend_request, created = FriendRequest.objects.get_or_create(
+        from_user=request.user, to_user=to_user
+    )
+
+    if created:
+        messages.success(request, f'Friend request sent to {to_user.username}.')
+    else:
+        messages.info(request, f'You have already sent a friend request to {to_user.username}.')
+
     return redirect('profile-view', username=to_user.username)
 
 
